cd59b71c2d6e916f2e2456f45713633a0bd62165
Vaishnavi-456 <vaishnavitile2210@gmail.com>
2024-01-31 18:28:08 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+
+import inquirer from "inquirer";
+import qr from 'qr-image';
+import fs from 'fs';
+
+inquirer
+.prompt([
+    {
+        message:"Type in URL",
+        name:"URL"
+    },
+]).then((answers) => {
+    const url = answers.URL;
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream('qr.png'));
+
+    fs.writeFile("URL.txt",url, (err) => {
+    if (err) throw err;
+    console.log("Data saved successfully")
+})  
+
+})
+
+
+.catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
+
+
